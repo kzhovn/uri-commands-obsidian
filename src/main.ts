378077e3f8b9e57be9d82a1c186156ca56e154e3
kzhovn <kzhovn@gmail.com>
2022-05-10 10:22:19 -0400

    Add relative path and valunt name placeholders #7

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,12 @@ const SELECTION_TEMPLATE = "{{selection}}";
 const FILE_TEXT_TEMPLATE = "{{fileText}}";
 const FILE_NAME_TEMPLATE = "{{fileName}}";
 const LINE_TEMPLATE = "{{line}}";
+const FILE_PATH_TEMPLATE = "{{filePath}}"
+const VAULT_NAME_TEMPLATE = "{{vaultName}}"
 const METADATA_REGEX = /{{meta:([^}]*)}}/; //note that this will *not* match if the metadata name has a } in it
 
 const editorTemplates = [SELECTION_TEMPLATE, LINE_TEMPLATE];  // templates that require an editor to extract
-const fileTemplates = [FILE_NAME_TEMPLATE, FILE_TEXT_TEMPLATE] // templates that require an active file (but not an editor)
+const fileTemplates = [FILE_NAME_TEMPLATE, FILE_TEXT_TEMPLATE, FILE_PATH_TEMPLATE]; // templates that require an active file (but not an editor)
 
 export default class URIPlugin extends Plugin {
 	settings: URIPluginSettings;
@@ -53,15 +55,15 @@ export default class URIPlugin extends Plugin {
 				const view: MarkdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
 				const file: TFile = this.app.workspace.getActiveFile();
 				const editor: Editor = view?.editor;
-				if (!editor) { 
+				if (!editor) { //don't show commands that require an editor
 					const uriContainsEditorTemplates = editorTemplates.some(template => command.URITemplate.includes(template)); //https://stackoverflow.com/a/66980203
-					if (uriContainsEditorTemplates) return false; // Command will only be available if an editor is
+					if (uriContainsEditorTemplates) return false;
 				}
 
-				if (!file) { // 
+				if (!file) { // don't show commands that require a file
 					const uriContainsFileTemplates = fileTemplates.some(template => command.URITemplate.includes(template));
 					if (uriContainsFileTemplates || METADATA_REGEX.test(command.URITemplate)) return false;
-				} else if (file.extension !== "md") {
+				} else if (file.extension !== "md") { // don't show commands that require a markdown file
 					if (command.URITemplate.includes(FILE_TEXT_TEMPLATE)) return false;
 				}
 
@@ -71,16 +73,42 @@ export default class URIPlugin extends Plugin {
 		});
 	}
 
-	/* rewrite all the below not to suck, I'm doing redudant checks and it's generally ugly as sin*/
-	
 	async runCommand(command: URICommand, editor?: Editor, file?: TFile) {
 		let uriString = command.URITemplate;
 
-		uriString = this.replaceName(uriString, file);
-		uriString = await this.replaceText(uriString, file);
-		uriString = await this.replaceMeta(uriString, file);
+		if(METADATA_REGEX.test(uriString)) { //specified metadata values
+			//checks if you can use metadata placeholder
+			if (!(this.app as any).plugins.plugins["metaedit"].api) {
+				new Notice("Must have MetaEdit enabled to use metadata placeholders")
+				return;
+			}
 
-		if (uriString === null) return;
+			//@ts-ignore
+			const {getPropertyValue} = this.app.plugins.plugins["metaedit"].api;
+
+			//for every instance of the placeholder: extract the name of the field, get the corresponding value, and replace the placeholder with the encoded value
+			//https://stackoverflow.com/questions/432493/how-do-you-access-the-matched-groups-in-a-javascript-regular-expression
+			let metadataMatch = METADATA_REGEX.exec(uriString); //grab a matched group, where match[0] is the full regex and match [1] is the (first) group
+
+			while (metadataMatch !== null) { //loop through all the matched until exec() isn't spitting out any more
+				let metadataValue = await getPropertyValue(metadataMatch[1], file);
+				if (!metadataValue) { //if this value doesn't exist on the file
+					new Notice(`The field ${metadataMatch[1]} does not exist on this file.`)
+					return;
+				}
+				uriString = replacePlaceholder(uriString, metadataMatch[0], metadataValue);
+				metadataMatch = METADATA_REGEX.exec(uriString);
+			}
+		}
+
+		if (uriString.includes(FILE_NAME_TEMPLATE)) { // base name of file
+			uriString = replacePlaceholder(uriString, FILE_NAME_TEMPLATE, file.basename);
+		}
+
+		if (uriString.includes(FILE_TEXT_TEMPLATE)) { //entire text of file
+			const fileText = await this.app.vault.read(file);
+			return replacePlaceholder(uriString, FILE_TEXT_TEMPLATE, fileText);
+		}
 
 		if (uriString.includes(SELECTION_TEMPLATE)) { //current selection
 			uriString = replacePlaceholder(uriString, SELECTION_TEMPLATE, editor.getSelection()); //currently replaced with empty string if no selection
@@ -91,82 +119,19 @@ export default class URIPlugin extends Plugin {
 			uriString = replacePlaceholder(uriString, LINE_TEMPLATE, editor.getLine(currentLine));
 		}
 
+		if (uriString.includes(FILE_PATH_TEMPLATE)) { //path inside the vault to the current file
+			uriString = replacePlaceholder(uriString, FILE_PATH_TEMPLATE, file.path);
+		}
+
+		if (uriString.includes(VAULT_NAME_TEMPLATE)) { //name of the current vault
+			uriString = replacePlaceholder(uriString, VAULT_NAME_TEMPLATE, this.app.vault.getName());
+		}
+
 		window.open(uriString);
 		if (this.settings.notification === true) {
 			new Notice(`Opening ${uriString}`);
 		}
 	}
-
-	//return URI with the {{fileName}} placeholders replaced with the name of the file, or null if the file is null
-	replaceName(uriString: string, file: TFile): string {
-		if (!uriString) return null;
-
-		if (uriString.includes(FILE_NAME_TEMPLATE)) { //note name (no path or extension)
-			if (file) {
-				return replacePlaceholder(uriString, FILE_NAME_TEMPLATE, file.basename);
-			} else {
-				new Notice("Must have active file to use {{fileName}} placeholder.")
-				return null;
-			}
-		} 
-
-		return uriString;
-	}
-
-	//returns URI with the {{fileText}} placeholders replaced with the text of the file, or null if it can't grab file text
-	async replaceText(uriString: string, file: TFile): Promise<string> {
-		if (!uriString) return null;
-		
-		if (uriString.includes(FILE_TEXT_TEMPLATE)) { //text of full file
-			if (file && file.extension === "md") {
-				const fileText = await this.app.vault.read(file);
-				return replacePlaceholder(uriString, FILE_TEXT_TEMPLATE, fileText);	
-			} else {
-				new Notice("Must have active markdown file to use {{fileText}} placeholder.")
-				return null;
-			}
-		}
-
-		return uriString;
-	}
-
-	//returns a URI with the {{meta:}} placeholders replaced with their corresponding values if I can grab metadata here
-	//null if it attempted an illegal operation, and the unmodified URI string if there are no {{meta:}} placeholders
-	async replaceMeta(uriString: string, file: TFile): Promise<string> {
-		if(METADATA_REGEX.test(uriString)) {
-			//checks if you can use metadata placeholder
-			//@ts-ignore
-			if (!this.app.plugins.plugins["metaedit"].api) {
-				new Notice("Must have MetaEdit enabled to use metadata placeholders")
-				return null;
-			} else if (!file) {
-				new Notice("Must have active file to use {{meta:}} placeholder.");
-				return null;
-			} else if (!uriString) {
-				return null;
-			}
-
-			//@ts-ignore
-			const {getPropertyValue} = this.app.plugins.plugins["metaedit"].api;
-			
-			//for every instance of the placeholder: extract the name of the field, get the corresponding value, and replace the placeholder with the encoded value
-			//https://stackoverflow.com/questions/432493/how-do-you-access-the-matched-groups-in-a-javascript-regular-expression
-			let metadataMatch = METADATA_REGEX.exec(uriString); //grab a matched group, where match[0] is the full regex and match [1] is the (first) group
-			
-			while (metadataMatch !== null) { //loop through all the matched until exec() isn't spitting out any more
-				let metadataValue = await getPropertyValue(metadataMatch[1], file);
-				if (!metadataValue) { //if this value doesn't exist on the file
-					new Notice(`The field ${metadataMatch[1]} does not exist on this file.`)
-					return null;
-				}
-				uriString = replacePlaceholder(uriString, metadataMatch[0], metadataValue);
-				metadataMatch = METADATA_REGEX.exec(uriString);
-			}
-		}
-
-		return uriString;
-	}
-
 
 	//from phibr0
 	private addFeatherIcons() {
@@ -179,8 +144,6 @@ export default class URIPlugin extends Plugin {
 }
 
 function replacePlaceholder(uriString: string, placeholder: string | RegExp, replacementString: string) {
-	if (uriString === null) return null;
-
 	const encodedReplacement = encodeURIComponent(replacementString);
 	return uriString.replace(placeholder, encodedReplacement);
 }
